8c9e7f1a39efb60c70747754984aa392e3025957
Mike Henry <62347522+MikeHenry-Simpon@users.noreply.github.com>
2020-04-02 16:01:34 +0400

    Add files via upload
    Le dossier du projet contenant les images et le code

---
+++ Projet 1/projet1.js
@@ -0,0 +1,7 @@
+var nombre = prompt('Ecrivez le Numero de votre Equipe');
+document.write("Toutes ces équipes sont geniales mais vous préférez L'"+ nombre);
+document.getElementById('1').innerHTML = 'Equipe N°1: <img src="/home/simplon/Documents/Projet 1/equipe1.png">';
+document.getElementById('2').innerHTML = 'Equipe N°2: <img src="/home/simplon/Documents/Projet 1/equipe2.png">';
+document.getElementById('3').innerHTML = 'Equipe N°3: <img src="/home/simplon/Documents/Projet 1/equipe3.png">';
+document.getElementById('4').innerHTML = 'Equipe N°4: <img src="/home/simplon/Documents/Projet 1/equipe4.png">';
+
